d1d39364eaff155a04402dd4c8fcbc2d4677fa98
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-11-18 13:13:57 +0530

    create a new funtional file for handle online orders

---
+++ utils/v2/paymentMethod/online.ts
@@ -0,0 +1,13 @@
+export default async function onlineSavePaymentData(paymentData) {
+
+
+  
+     
+
+     
+
+
+    console.log('Saving payment data:', paymentData);
+    console.log('i did it mtf');
+  }
+  
